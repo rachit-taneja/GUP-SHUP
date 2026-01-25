0412dcf89521f9084846d9c65208ae8c58f2d619
rachit-taneja <tanejarachit111@gmail.com>
2026-01-25 23:33:24 +0530

    New changes

--- backend/server.js
+++ backend/server.js
@@ -3,3 +3,11 @@ const express =require('express');
 
 const app=express();
 
+const PORT=5000;
+app.get('/',(req,res,next )=>{
+    res.send('Hello, World!');
+});
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
