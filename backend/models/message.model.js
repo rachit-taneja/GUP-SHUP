bc3a2f291cbff213df8d5f64c7c319fbe2e4a676
rachit-taneja <tanejarachit111@gmail.com>
2026-02-25 00:48:32 +0530

    New changes

--- backend/models/message.model.js
+++ backend/models/message.model.js
@@ -11,7 +11,7 @@ const messageSchema = new mongoose.Schema({
         ref: 'User',
         required: true 
     },
-    content: {
+    message: {
         type: String,
         required: true
     }
