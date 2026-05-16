366171484ed570d16283e8f657f3e5cdeb9cd2df
rachit-taneja <tanejarachit111@gmail.com>
2026-05-17 01:58:58 +0530

    New changes in DB

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,7 +1,7 @@
 import User from "../models/user.model.js";
 import { asynchandler } from "../utilities/asynchandler.js";
 import Errorhandler from "../utilities/errorhandler.js";
-
+import bcrypt, { hash } from "bcryptjs";
 export const register = asynchandler(async (req, res, next) => {
 
   const { name, username, email, password, gender } = req.body;
@@ -17,21 +17,50 @@ export const register = asynchandler(async (req, res, next) => {
   if (user) {
     return next(new Errorhandler("User already exists", 400));
   }
-
-const avatartype = gender==="male" ? "male" : "female";
+  const hashedpassword= await bcrypt.hash(password,10);
+  const avatartype = gender==="male" ? "male" : "female";
   const avatar = `https://avatar.iran.liara.run/public/$(avatartype)?username=(username)`;
 
   const newUser = await User.create({
     name,
     username,
     email,
-    password,
-    avatar,
+    password: hashedpassword,
+    gender,
+    avatar
   });
-
+  
   return res.status(201).json({
     success: true,
     message: "User registered successfully",
     newUser,
+  });
+});
+
+export const login = asynchandler(async (req, res, next) => {
+
+  const { username,  password } = req.body;
+
+  // console.log(name, username, email, password, gender);
+
+  if (!username || !password) {
+    return next(new Errorhandler("Please provide all required fields", 400));
+  }
+
+  const user = await User.findOne({ username });
+
+  if (!user) {
+    return next(new Errorhandler("Invalid credentials", 401));
+  }
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    return next(new Errorhandler("Invalid credentials", 401));
+  }
+  
+
+  return res.status(201).json({
+    success: true,
+    message: "User signed in successfully",
+    user,
   });
 });
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,9 +1,9 @@
 import express from 'express';
-// import { login } from '../controllers/user.controller.js';
+import { login } from '../controllers/user.controller.js';
 import { register } from '../controllers/user.controller.js';
 const router=express.Router();
 
 router.post('/register',register);
-// router.post('/login',login);
+router.post('/login',login);
 
 export default router;
