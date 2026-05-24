412a31c8184936865621c1f3329caf1f0b9bdfc5
rachit-taneja <tanejarachit111@gmail.com>
2026-05-24 19:05:29 +0530

    New changes

---
+++ backend/middlewares/auth.js
@@ -0,0 +1,7 @@
+export const register = asynchandler(async (req, res, next) => {
+
+  const token = req.cookies.token;
+
+  if (!token) {
+    return next(new Errorhandler("You are already logged in", 400));
+  }
