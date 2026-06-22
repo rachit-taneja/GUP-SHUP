f5ef932426ad8177f25d9c69a12895b2991c06ef
rachit-taneja <tanejarachit111@gmail.com>
2026-06-22 19:09:20 +0530

    New changes

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -3,45 +3,41 @@ import conversation from "../models/converstion.model.js";
 import { asynchandler } from "../utilities/asynchandler.js";
 import Errorhandler from "../utilities/errorhandler.js";
 
-
 export const sendMessage = asynchandler(async (req, res, next) => {
-         
-    const senderID=req.user._id;
-    const receiverID=req.params.receiverID;
-    const message=req.body.message;
+  const senderID = req.user._id;
+  const receiverID = req.params.receiverID;
+  const message = req.body.message;
 
-    console.log(senderID,receiverID,message);
-    
-    if(!message || !senderID || !receiverID){
-        return next(new Errorhandler("All fields are required",400));
-    }
+  console.log(senderID, receiverID, message);
 
-    const conversation= await conversation.findOne({
-        participants: { $all: [senderID, receiverID] }
+  if (!message || !senderID || !receiverID) {
+    return next(new Errorhandler("All fields are required", 400));
+  }
+
+  const conversation = await conversation.findOne({
+    participants: { $all: [senderID, receiverID] },
+  });
+
+  if (!conversation) {
+    const newConversation = await conversation.create({
+      participants: [senderID, receiverID],
     });
+  }
 
-    if(!conversation){
-        const newConversation= await conversation.create({
-            participants:[senderID,receiverID]  })
-        }
+  const newMessage = await Message.create({
+    senderID,
+    receiverID,
+    message,
+  });
 
+  if (newMessage) {
+    conversation.messages.push(newMessage._id);
+    await conversation.save();
+  }
 
-
-        const newMessage= await Message.create({
-            senderID,
-            receiverID,
-            message
-
-        });
-
-        if(newMessage){
-            conversation.messages.push(newMessage._id);
-            await conversation.save();
-        }
-
-        return res.status(201).json({
-            success:true,
-            message:"Message sent successfully",
-            newMessage
-        });
+  return res.status(201).json({
+    success: true,
+    message: "Message sent successfully",
+    newMessage,
+  });
 });
