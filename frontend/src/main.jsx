0d0a3ee099f2700d5aef8ee475131a7ac806a450
rachit-taneja <tanejarachit111@gmail.com>
2026-06-30 23:01:34 +0530

    Installed and setup cors and axios

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,8 +7,10 @@ import Signup from './components/Signup.jsx'
 import Login from './components/Login.jsx'  
 import Home from './components/home/Home.jsx'
 import homeScreen from './components/homescreen.jsx'
-// import { store } from './store/store'
-// import { Provider } from 'react-redux'
+
+import { store } from './store/store'
+import React from 'react'
+import { Provider } from 'react-redux'
 const router = createBrowserRouter([
   {
     path: '/text',
@@ -25,12 +27,12 @@ const router = createBrowserRouter([
 ])
 createRoot(document.getElementById('root')).render(
   <>
-  {/* <Provider store={store}> */}
+  <React.StrictMode>
+   <Provider store={store}>
   <App />
   <RouterProvider router={router} />
- {/* </Provider> */}
+  </Provider>   
  
-  
+  </React.StrictMode>
 </>
-
 )
--- frontend/src/store/slice/user.thunk.js
+++ frontend/src/store/slice/user.thunk.js
@@ -2,17 +2,21 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axiosInstance from "../../components/utilities/axiosInstance";
 
 export const fetchUserThunk = createAsyncThunk(
-    'user/fetchUser',
-    async (username, password) , (rejectWithValue)=> {
-        try {
-            const response = await axiosInstance.post('/user/login', {
-                username,
-                password
-            });
-            return response.data;
-            
-        } catch (error) {
-            console.error('Error fetching user:', error);
-            return rejectWithValue(error.response.data);
-        }   }
+  "user/fetchUser",
+  async ({ username, password }, { rejectWithValue }) => {
+    try {
+      const response = await axiosInstance.post("/user/login", {
+        username,
+        password,
+      });
+
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching user:", error);
+
+      return rejectWithValue(
+        error.response?.data || error.message
+      );
+    }
+  }
 );
